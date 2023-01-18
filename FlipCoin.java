c92f388d6d54387fec5d34df29c5c670aca73ba8
jyoti2y5 <jyotiyadav46408@gmail.com>
2023-01-18 19:16:44 +0530

    this is day 5

--- FlipCoin.java
+++ FlipCoin.java
@@ -1,5 +1,6 @@
 public class FlipCoin {
     public static void main(String[] args){
+        System.out.println("hello");
         if(Math.random() <0.5){
             System.out.println("Head");
         }
